4f9f6e8d6bda88fcbefb2a47b347e852e425ed16
unknown <johnvictordml@gmail.com>
2024-04-13 12:36:59 +0100

    created and made other pages responsive

--- js/app.js
+++ js/app.js
@@ -15,6 +15,28 @@ submenuItems.forEach(item => {
     });
 });
 
+// Gallery Modal
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalImg");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+var images = document.querySelectorAll(".gallery-image-cont img");
+
+images.forEach(function(img, index) {
+  img.addEventListener("click", function() {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  });
+});
+
+
+// to close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+
 
 // google translate code
 function googleTranslateElementInit() {
